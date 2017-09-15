e7c595682cbf4ecf583ee142a868c0b1b6f74195
maxbrenner <maxbrenner110@gmail.com>
2017-09-15 14:04:24 -0400

    Updated Example

--- example.py
+++ example.py
@@ -1,10 +1,11 @@
 from agents.dqn import DQN
 from policy import EpsilonGreedyPolicy as EGP
+from callbacks import *
+from benchmark import Benchmark
 from keras.models import Sequential
 from keras.layers import *
 from keras.optimizers import Adam
 import gym
-from callbacks import *
 
 '''
 This is an example of how to use these classes in conjunction with gym and keras
@@ -15,7 +16,7 @@ env = gym.make('CartPole-v1')
 state_size = env.observation_space.shape[0]
 action_size = env.action_space.n
 
-# Make the model, use Keras
+# Make the model, use Keras (Sequential or Functional Model)
 model = Sequential()
 model.add(Dense(24, input_dim=state_size, activation='relu'))
 model.add(Dense(24, activation='relu'))
@@ -25,14 +26,15 @@ optimizer = Adam(lr=1e-2)
 
 # Make the policy and agent
 policy = EGP(0.95, 0.01, decay=0.003)
-
-agent = DQN(double_dqn=True, model=model, optimizer=optimizer, policy=policy, gamma=0.95, target_model_update_policy='soft', target_model_hard_policy_wait=200, target_model_soft_policy_constant=0.9, max_memory_length=1000)
+agent = DQN(double_dqn=True, dueling_dqn=False, model=model, optimizer=optimizer, policy=policy, gamma=0.95, target_model_update_policy='soft', target_model_hard_policy_wait=200, target_model_soft_policy_constant=0.9, max_memory_length=1000)
 
 # Make the callbacks
 rew_cb = PrintReward()
 eps_cb = PrintEpsilon(episodic=True, iterations=None)
 
+# Make Benchmark if you want to save agent test data
+benchmark = Benchmark('test_benchmark', episode_iteration=1)
+
 # Tran and Test
 agent.train(env, 10000, 500, print_rew_cb=rew_cb, print_eps_cb=eps_cb, visualize=False, allow_printing=True)
-
-agent.test(env, 1000, 500, print_rew_cb=rew_cb, visualize=False, allow_printing=True)
+agent.test(env, 1000, 500, print_rew_cb=rew_cb, benchmark=benchmark, visualize=True, allow_printing=True)
