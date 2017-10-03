8ef5399b0cb6c590b984cd36e80495942c73ab8e
maxbrenner <maxbrenner110@gmail.com>
2017-10-02 23:16:57 -0400

    Added replay period wait

--- agents/base.py
+++ agents/base.py
@@ -8,7 +8,9 @@ import keras.backend as K
 
 
 class Agent:
-    def __init__(self, model, optimizer, policy, state_dim, action_size, gamma=0.95, target_model_update_policy='soft', target_model_hard_policy_wait=1000, target_model_soft_policy_constant=0.9, frame_skip=0, reward_clipping=False):
+    def __init__(self, model, optimizer, policy, state_dim, action_size, gamma=0.95, target_model_update_policy='soft',
+                 target_model_hard_policy_wait=1000, target_model_soft_policy_constant=0.9, replay_period_wait=1,
+                 reward_clipping=True):
         if target_model_hard_policy_wait < 1:
             raise ValueError('`target_model_hard_policy_wait` is < 1.')
         if gamma < 0 or gamma > 1:
@@ -30,7 +32,7 @@ class Agent:
         self.target_model_update_policy = target_model_update_policy
         self.target_model_hard_policy_wait = target_model_hard_policy_wait
         self.target_model_soft_policy_constant = target_model_soft_policy_constant
-        self.frame_skip = frame_skip
+        self.replay_period_wait = replay_period_wait
         self.reward_clipping = reward_clipping
 
         self.uses_replay = None
@@ -64,9 +66,11 @@ class Agent:
                 self.tar_model.set_weights(self.beh_model.get_weights())
         else:
             tau = self.target_model_soft_policy_constant
-            self.tar_model.set_weights(tau * np.asarray(self.beh_model.get_weights()) + (1 - tau) * np.asarray(self.tar_model.get_weights()))
+            self.tar_model.set_weights(tau * np.asarray(self.beh_model.get_weights()) + (1 - tau) *
+                                       np.asarray(self.tar_model.get_weights()))
 
-    def train(self, env, nb_steps, nb_steps_ep_max=None, print_rew_cb=None, print_eps_cb=None, visualize=False, allow_printing=True):
+    def train(self, env, nb_steps, nb_steps_ep_max=None, print_rew_cb=None, print_eps_cb=None, visualize=False,
+              allow_printing=True):
         nb_steps_ep_max_in = nb_steps_ep_max
         if nb_steps_ep_max is not None:
             nb_steps_ep_max_in = nb_steps_ep_max - 1
@@ -75,15 +79,18 @@ class Agent:
             self.policy.set_cb(print_eps_cb)
             send_eps = print_eps_cb
         self.currently_used_policy = self.policy
-        runner = Runner(RunType.TRAIN, self, env, nb_steps, nb_steps_ep_max_in, print_rew_cb, send_eps, None, visualize, allow_printing)
+        runner = Runner(RunType.TRAIN, self, env, nb_steps, nb_steps_ep_max_in, print_rew_cb, send_eps, None, visualize,
+                        allow_printing)
         runner.run()
 
-    def test(self, env, nb_steps, nb_steps_ep_max=None, print_rew_cb=None, benchmark=None, visualize=False, allow_printing=True):
+    def test(self, env, nb_steps, nb_steps_ep_max=None, print_rew_cb=None, benchmark=None, visualize=False,
+             allow_printing=True):
         nb_steps_ep_max_in = nb_steps_ep_max
         if nb_steps_ep_max is not None:
             nb_steps_ep_max_in = nb_steps_ep_max - 1
         # The policy auto switches to greedy when testing so no epsilon cb even allowed
-        runner = Runner(RunType.TEST, self, env, nb_steps, nb_steps_ep_max_in, print_rew_cb, None, benchmark, visualize, allow_printing)
+        runner = Runner(RunType.TEST, self, env, nb_steps, nb_steps_ep_max_in, print_rew_cb, None, benchmark, visualize,
+                        allow_printing)
         runner.run()
 
     def summary(self):
@@ -101,6 +108,6 @@ class Agent:
             text += "Target model update wait: {}\n".format(self.target_model_hard_policy_wait)
         else:
             text += "Target model update constant: {}\n".format(self.target_model_soft_policy_constant)
-        text += 'Frame skip: {}\n'.format(self.frame_skip)
+        text += 'Replay period wait: {}\n'.format(self.replay_period_wait)
         text += 'Reward clipping: {}\n'.format(self.reward_clipping)
         return text
